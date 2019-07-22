c234852bb1be4cb59ddd0947773cc0a2847c512d
irhamsahbana <irhamsahbana@gmail.com>
2019-07-22 14:41:29 +0800

    Revert "add 'update' pengurus dmmif and hmif modul"
    This reverts commit 273aefc51d83fd91e7068efb2c1dcc7f33098f64.

--- application/views/admin/dashboard/pengurus.php
+++ application/views/admin/dashboard/pengurus.php
@@ -9,7 +9,6 @@
         $tipe_pengurus = '';
         $folder = '';
         $modul = $this->uri->segment(2);
-        $id = '';
         
         if($modul == 'pengurusDmmif'){
             $tipe_pengurus = 1;
@@ -33,12 +32,12 @@
                          <?php echo $judul; ?>
                         </h4>
                         <h6 style="color: yellow;">
-                           <?php
-                                $info = $this->session->flashdata('info');
-                                if(!empty($info)){
-                                    echo $info;
-                                }
-                            ?>
+                               <?php
+                                    $info = $this->session->flashdata('info');
+                                    if(!empty($info)){
+                                        echo $info;
+                                    }
+                                ?>
                         </h6>
 
                         <button type="button" data-toggle="modal" data-target="#create_modal" data-whatever="@getbootstrap" class="btn mb-1 btn-success">Tambah<span class="btn-icon-right"><i class="fa fa-plus-square"></i></span>
@@ -70,14 +69,14 @@
                                         <td style="vertical-align: middle;text-align: center;"><?php echo $get->nama_pengurus ?></td>
                                         <td style="vertical-align: middle;text-align: left;"><?php echo $get->jabatan_pengurus ?></td>
                                         <td style="vertical-align: middle;text-align: center;">
-                                            <button type="button" class="btn mb-1 btn-primary" data-toggle="modal" data-target="#update_modal<?= $id ?>"><i class="fa fa-pencil"></i>
+                                            <button type="button" class="btn mb-1 btn-primary" data-toggle="modal" data-target="#update_modal"><i class="fa fa-pencil"></i>
                                             </button>
                                             <button type="button" class="btn mb-1 btn-danger" data-toggle="modal" data-target="#delete_modal<?= $id ?>"><i class="fa fa-trash"></i>
                                             </button>
                                         </td>
-                                    </tr> 
+                                    </tr>
+                                <?php } ?> 
                                 </tbody>
-                                <?php } ?>
                             </table>
                         </div>
                     </div>
@@ -88,7 +87,7 @@
     <!-- #/ container -->
 </div> 
 
-<!-- Begin Add Modal -->
+<!-- Begin Modal Add -->
 <div class="modal fade" id="create_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -141,9 +140,9 @@
         </div>
     </div>
 </div>
-<!-- End Add Modal -->
+<!-- End Modal Add -->
 
-<!-- Begin Delete Modal -->
+<!-- Begin Modal Delete -->
 <?php
    foreach($data as $get){
     $id = $get->id_pengurus;
@@ -181,67 +180,4 @@
     </div>                                     
 </div>
 <?php } ?>
-<!-- End Delete Modal -->
-
-<!-- Begin Update Modal -->
-<?php
-    foreach($data as $get){
-    $id = $get->id_pengurus;
-?>
-
-<div class="modal fade" id="update_modal<?= $id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Informasi Pengurus <?php echo $pengurus?></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="<?php echo site_url('admin/doUpdatePengurus/'.$tipe_pengurus.'/'.$id) ?>" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="nama-lengkap" class="col-form-label">Nama Lengkap:</label>
-                        <input type="text" class="form-control" name="nama_pengurus" placeholder="Masukkan nama lengkap pengurus" value="<?php echo $get->nama_pengurus ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="jabatan" class="col-form-label">Jabatan:</label>
-                        <input type="text" class="form-control" name="jabatan" placeholder="Ex. Ketua Umum HMIF FT-UH" value="<?php echo $get->jabatan_pengurus ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="periode" class="col-form-label">Periode:</label>
-                        <input type="text" class="form-control" name="periode" placeholder="Ex. Masa Bakti 2019" value="<?php echo $get->periode_pengurus ?>" required>
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend"><span class="input-group-text">Facebook</span>
-                        </div>
-                        <input type="text" class="form-control" name="facebook" placeholder="Masukkan Link akun facebook" value="<?php echo $get->facebook ?>">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend"><span class="input-group-text">Twitter</span>
-                        </div>
-                        <input type="text" class="form-control" name="twitter" placeholder="Masukkan Link akun twitter" value="<?php echo $get->twitter ?>">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend"><span class="input-group-text">Instagram</span>
-                        </div>
-                        <input type="text" class="form-control" name="instagram" placeholder="Masukkan Link akun instagram" value="<?php echo $get->instagram ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="file" class="form-control-file" name="fotoPengurus" accept=".png, .jpg, .jpeg">
-                        <div style="font-size: 10px">
-                            File hanya JPG dan PNG dengan ukuran Maks. 2048 Kb
-                        </div>
-                    </div>
-                    <input type="hidden" readonly name="tipe_pengurus" value="<?php echo $tipe_pengurus ?>">
-                    <input type="hidden" readonly name="foto_lama" value="<?= $get->foto_pengurus ?>">
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Update</button>
-                    </div>
-                </form>
-            </div>  
-        </div>
-    </div>
-</div>
-
-<?php } ?>
-<!-- End Update Modal -->
+<!-- End Modal Delete -->
