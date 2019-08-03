cef5e004734984f7fde73e7db3a251f6ae6b02af
irhamsahbana <irhamb4ckup@gmail.com>
2019-08-03 15:06:03 +0800

    Revert "[ADD] Galeri.php"
    This reverts commit 0847a99dd0971ad90a940e1f977edbab90587d46.

--- application/views/admin/dashboard/galeri.php
+++ application/views/admin/dashboard/galeri.php
@@ -3,7 +3,7 @@
         ***********************************-->
         <div class="content-body">
             <!-- row -->
-			<!-- upload -->
+
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
