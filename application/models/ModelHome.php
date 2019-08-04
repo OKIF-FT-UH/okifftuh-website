6e3cece3190092b984f1896d759494d64270093d
GilbertHG <gilbertgoes11@gmail.com>
2019-08-04 22:16:01 +0800

    Fix Pengurus

--- application/models/ModelHome.php
+++ application/models/ModelHome.php
@@ -2,5 +2,6 @@
 class ModelHome extends CI_Model {
 
     public function infoKini($t,$l,$f,$o,$ad){return $this->db->limit($l,$f)->order_by($o,$ad)->get($t)->result();}
+    public function pengurus($t,$w,$l,$f,$o,$ad){return $this->db->where($w)->limit($l, $f)->order_by($o,$ad)->get($t)->result();}
     
 }
