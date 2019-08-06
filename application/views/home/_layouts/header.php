29f2c1e242ef24b5641419aa8778e25a8c5a0d3e
GilbertHG <gilbertgoes11@gmail.com>
2019-08-06 16:37:36 +0800

    Revert "Revert "Revert "sejarahPengurus"""
    This reverts commit 55e68da61a903088752ed09764ef59d8fd397a0a.

--- application/views/home/_layouts/header.php
+++ application/views/home/_layouts/header.php
@@ -69,7 +69,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
                                 <li><a href="<?= base_url('Home/pengurusDmmif') ?>" class="drop-text">DMMIF FT-UH</a></li>
                                 <li><a href="<?= base_url('Home/pengurusHmif') ?>" class="drop-text">HMIF FT-UH</a></li>
-                                <li><a href="<?= base_url('Home/sejarahPengurus') ?>" class="drop-text">Sejarah Kepengurusan</a></li>
                             </ul>
                         </li>
 
