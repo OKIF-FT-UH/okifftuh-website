84459276a4cf71e0209f981450bd44f315029e13
GilbertHG <gilbertgoes11@gmail.com>
2019-09-27 19:50:50 +0800

    program Kerja

--- application/views/home/dashboard/programKerja.php
+++ application/views/home/dashboard/programKerja.php
@@ -13,8 +13,8 @@
                             <h3><span class="small">April-Mei</span> Bakti Sosial</h3>
                         </div>
                         <div class="body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
-                            <img src="images/banner1.jpg" alt="infinitude" class="img-fluid">
+                            <p>Bakti Sosial Informatika sebuah program kerja HMIF FT-UH yang bertujuan untuk mengajarkan kader-kader OKIF FT-UH untuk saling peduli terhadap sesama serta menambah wawasan dan pengetahuan sosial kader-kader OKIF FT-UH.</p>
+                            <img src="<?= base_url('assets/home/proker/bakti-sosial.png')?>" alt="infinitude" class="img-fluid">
                         </div>
                     </div>
 
@@ -26,8 +26,8 @@
                             <h3><span class="small">April-Juni</span>Informatics Website</h3>
                         </div>
                         <div class="body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
-                            <img src="images/banner2.jpg" alt="infinitude" class="img-fluid">
+                            <p>Informatic Website sebuah program kerja HMIF FT-UH yang bertujuan agar OKIF FT-UH memiliki pusat sistem informasi yang dapat menyediakan segala informasi yang terkait dengan OKIF FT-UH.</p>
+                            <img src="<?= base_url('assets/home/proker/informatics-website.png')?>" alt="infinitude" class="img-fluid">
                         </div>
                     </div>
 
@@ -39,8 +39,8 @@
                             <h3><span class="small">Juli</span>Informatics Tour</h3>
                         </div>
                         <div class="body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
-                            <img src="images/banner3.jpg" alt="infinitude" class="img-fluid">
+                            <p>Informatics Tour adalah program kerja HMIF FT-UH merupakan wisata yang bertujuan mengenalkan lingkup kerja nyata yang berhubungan dengan keiinformatikaan.</p>
+                            <img src="<?= base_url('assets/home/proker/informatics-tour.png')?>" alt="infinitude" class="img-fluid">
                         </div>
                     </div>
 
@@ -52,8 +52,8 @@
                             <h3><span class="small">Juli-Agustus</span>Informatics Awards</h3>
                         </div>
                         <div class="body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
-                            <img src="images/banner4.jpg" alt="infinitude" class="img-fluid">
+                            <p>Informatics Awards adalah program kerja HMIF FT-UH yang bertujuan sebagai wadah dalam menumbuhkan serta memfasilitasi minat dan bakat anggota OKIF FT-UH </p>
+                            <img src="<?= base_url('assets/home/proker/informatics-award.png')?>" alt="infinitude" class="img-fluid">
                         </div>
                     </div>
 
@@ -65,8 +65,8 @@
                             <h3><span class="small">Juli-September</span>INCEPTION</h3>
                         </div>
                         <div class="body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
-                            <img src="images/banner1.jpg" alt="infinitude" class="img-fluid">
+                            <p>INCEPTION merupakan kegiatan skala nasional HMIF FT-UH berupa lomba karya tulis ilmiah menumbuhkembangkan karya tulis mahasiswa dalam bentuk penuangan gagasan atau ide kreatif yang bersifat orisinil, visioner dan implementatif untuk mencari solusi atas permasalahan yang ada dengan pendekatan ilmu-ilmu informatika.</p>
+                            <img src="<?= base_url('assets/home/proker/inception.png')?>" alt="infinitude" class="img-fluid">
                         </div>
                     </div>
 
